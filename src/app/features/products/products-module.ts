940249de1100385a52d38a7149dc83050d526aa7
Job2x <eslamahmed007x>
2026-04-06 21:01:05 +0200

    Changes to be committed: 	modified:   src/app/features/home/landing/landing.html 	modified:   src/app/features/home/landing/landing.ts 	modified:   src/app/features/products/product-details/product-details.html 	modified:   src/app/features/products/products-module.ts 	modified:   src/app/layout/navbar/navbar.html 	modified:   src/app/shared/components/blob-background/blob-background.html 	modified:   src/styles.css

--- src/app/features/products/products-module.ts
+++ src/app/features/products/products-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list';
 import { ProductDetailsComponent } from './product-details/product-details';
+import { BlobBackgroundComponent } from '../../shared/components/blob-background/blob-background';
 
 @NgModule({
   declarations: [ProductListComponent, ProductDetailsComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, BlobBackgroundComponent],
 })
 export class ProductsModule {}
