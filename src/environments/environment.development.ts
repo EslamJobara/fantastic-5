1492aa4ee059e4e14bc96d4fd307881cd1c3638b
Eslam Jobara <jobaraeslam@gmail.com>
2026-05-04 19:17:34 +0300

    Changes to be committed: 	modified:   src/environments/environment.development.ts 	modified:   src/environments/environment.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e-commerce-beige-iota.vercel.app/api'
+  apiUrl: 'https://e-commerce-beige-iota.vercel.app/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://e-commerce-a6cz.beige.com/api',
+  production: true,
+  apiUrl: 'https://e-commerce-beige-iota.vercel.app/api',
 };
